3ea5c068c34aa7084b141c5b7f7861fab27e1952
DogTagRam <47109530+LongLiveRam@users.noreply.github.com>
2024-02-24 17:15:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# phplibrarysystem
+ Another project for my prepartion of Laravel Deep Dive (coded with MVC files structure and other code improvements)
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Library System</title>
+  <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+  <nav>
+    <div class="logo">
+      <h1>Library Management System</h1>
+    </div>
+    <ul>
+      <li>Home</li>
+      <li>Listed Books</li>
+      <li>Unlisted Books</li>
+      <li>Account</li>
+    </ul>
+  </nav>
+
+  <header>
+    <h2>Welcome </h2> <!-- PHP Global Variable for username -->
+  </header>
+
+  <main>
+    
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
+</html>
