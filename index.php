4f5ea56aa727aebe40a66524dd06d8049d22c36e
DogTagRam <47109530+LongLiveRam@users.noreply.github.com>
2024-02-25 11:01:14 +0800

    Init index.php and css
    Initial index and css in preparation for the PHP partials

--- index.php
+++ index.php
@@ -11,24 +11,36 @@
     <div class="logo">
       <h1>Library Management System</h1>
     </div>
-    <ul>
-      <li>Home</li>
-      <li>Listed Books</li>
-      <li>Unlisted Books</li>
-      <li>Account</li>
+    <ul class="nav-items">
+      <li><a href="/">Home</a></li>
+      <li><a href="/">Listed Books</a></li>
+      <li><a href="/">Unlisted Books</a></li>
+      <li><a href="/">Account</a></li>
     </ul>
   </nav>
 
   <header>
-    <h2>Welcome </h2> <!-- PHP Global Variable for username -->
+    <h2>Welcome User</h2> <!-- PHP Global Variable for username -->
   </header>
 
   <main>
-    
+
+    <section class="dashboard">
+      <div>
+        <h3>Books Listed</h3>
+      </div>
+      <div>
+        <h3>Books Sold</h3>
+      </div>
+      <div>
+        <h3>Books Total Stock: </h3>
+      </div>
+    </section>
+
   </main>
 
   <footer>
-
+    <h4>Copyright @ 2024 Ramel Montera</h4>
   </footer>
 </body>
 </html>
